7fd52dcf9533d842976b3a3eae47928b3c595f06
Yakov Rogov <rogov@haulmont.com>
2020-03-08 16:45:05 +0400

    added function to get reversed heads from start of the function to address

---
+++ src/utils/get_heads_to_address.py
@@ -0,0 +1,7 @@
+from function_addresses import get_function_start
+import idautils
+
+
+def get_heads_to_address(address):
+    start = get_function_start(address)
+    return reversed(list(idautils.Heads(start, address)))
